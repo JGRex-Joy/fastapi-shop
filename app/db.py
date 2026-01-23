384b648f648abd51476b1c7eb6c2854a97311b8e
JGRex-Joy <omurkulovamir2008@gmail.com>
2026-01-23 14:22:37 +0600

    making psql

--- app/db.py
+++ app/db.py
@@ -5,8 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
 engine = create_engine(
-    settings.database_url,
-    connect_args={"check_same_thread": False}
+    settings.database_url
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
