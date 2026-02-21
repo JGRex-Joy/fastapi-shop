179f8faea2a47ed946f900ec9c391543faa21775
JGRex-Joy <omurkulovamir2008@gmail.com>
2026-02-21 14:00:07 +0600

    deploying

--- app/config.py
+++ app/config.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     app_name: str = "FastAPI Shop"
     debug: bool = True
     database_url: str = os.getenv("DATABASE_URL")
-    cors_origins: list = ["*"]
     
     class Config:
         env_file = ".env"
--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origins=["https://jgrex-joy.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
